968fa3c94c1f28eb563de9fe70c97896b5f33e35
Rora Alem <rora.goitom@gmail.com>
2024-03-28 22:42:34 -0400

    Implement form validation and handle form submission

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,31 @@
+import { useFormik } from "formik";
+
+// validation function to check the email field
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = "Valid email required"; // Error message if email is empty
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Valid email required"; // Error message if email format is invalid
+  }
+  return errors;
+};
 
 function SignUp(props) {
+  //Initialize Formik with inital values, validation function and submit handler
+  const formik = useFormik({
+    initialValues: {
+      email: "", // Initial value for the email field
+    },
+    validate, // validation function to validate form fields
+    onSubmit: (values) => {
+      // alert(JSON.stringify(values, null, 2)); //show form values in an alert
+      props.setSuccessModal(true); //update state to show success modal
+      props.setEmail(values.email); // Update email state with user input
+    },
+  });
+
   return (
     <div className="flex flex-col md:h-screen justify-center items-center">
       <div
@@ -32,16 +58,27 @@ function SignUp(props) {
             </ul>
           </div>
           <div className="md:p-4">
-            <form onSubmit={formik.handleSubmit} autoComplete="off">
+            <form onSubmit={formik.handleSubmit} autoComplete="off ">
               <div className="text-[12px] font-bold flex justify-between mt-4">
                 <label htmlFor="email">Email address</label>
+                {/* Display error message if email field is touched and has an error */}
+                {formik.touched.email && formik.errors.email ? (
+                  <span className="text-redish ">{formik.errors.email}</span>
+                ) : null}
               </div>
               <input
                 name="email"
                 id="email"
                 type="email"
-                className="w-full p-4 border rounded-lg my-2 outline:none focus:border-slate-700 bg-rose-100 border-rose-400"
+                className={`w-full p-4 border rounded-lg my-2 outline:none focus:border-slate-700 ${
+                  formik.touched.email && formik.errors.email
+                    ? "bg-rose-100 border-rose-400"
+                    : ""
+                }`}
                 placeholder="email@company.com"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
               />
 
               <button
